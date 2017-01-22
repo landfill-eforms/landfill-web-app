f9c72928f5108f5cc417777592780a0d0a69b541
alvinquach <alvinquach91@gmail.com>
2017-01-22 14:29:12 -0800

    copied instantaneous data classes from old repo, untested

---
+++ server/src/main/java/org/lacitysan/landfill/server/datamap/mapper/InstantaneousDataMapper.java
@@ -0,0 +1,12 @@
+package org.lacitysan.landfill.server.datamap.mapper;
+
+/**
+ * @author Alvin Quach
+ */
+public class InstantaneousDataMapper {
+	
+	public static void map() {
+		
+	}
+
+}
---
+++ server/src/main/java/org/lacitysan/landfill/server/persistence/entity/Instrument.java
@@ -0,0 +1,69 @@
+package org.lacitysan.landfill.server.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.lacitysan.landfill.lib.enumeration.InstrumentType;
+
+/**
+ * @author Alvin Quach
+ */
+@Entity
+@Table(name="test.dbo.Instruments")
+public class Instrument {
+	
+	@Id
+	@Column(name="InstrumentPK")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotNull
+	private String serialNumber;
+	
+	@NotNull
+	private String name;
+	
+	@Column(name="InstrumentTypeFK")
+	@Enumerated(EnumType.ORDINAL)
+	private InstrumentType instrumentType;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public InstrumentType getInstrumentType() {
+		return instrumentType;
+	}
+
+	public void setInstrumentType(InstrumentType instrumentType) {
+		this.instrumentType = instrumentType;
+	}
+
+}

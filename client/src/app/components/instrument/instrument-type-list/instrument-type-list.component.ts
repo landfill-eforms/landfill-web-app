f3f46c69f719a99bc0ff7f454b7984c336c0786e
alvinquach <alvinquach91@gmail.com>
2017-03-08 09:59:55 -0800

    save

--- client/src/app/components/instrument/instrument-type-list/instrument-type-list.component.ts
+++ client/src/app/components/instrument/instrument-type-list/instrument-type-list.component.ts
@@ -1,3 +1,5 @@
+import { Sort } from './../../../utils/sort.utils';
+import { InstrumentType } from './../../../model/server/persistence/entity/instrument/instrument-type.class';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './instrument-type-list.component.html',
     styleUrls: ['./instrument-type-list.component.scss']
 })
-export class InstrumentTypeListComponent {
+export class InstrumentTypeListComponent implements OnInit {
+
+    isDataLoaded:boolean;
+    loadingMessage:string;
+    instrumentTypeList:InstrumentType[];
+    
+    sort:Sort = {
+		current: "id",
+		reversed: false
+	}
+
+	sortProperties:any = {
+
+	}
+
+    ngOnInit() {
+		this.loadingMessage = "Loading Equipment Types...";
+		this.loadInstrumentTypes();
+	}
 	
+    loadInstrumentTypes() {
+
+    }
+
 }
--- client/src/app/components/instrument/instrument-type/instrument-type.component.ts
+++ client/src/app/components/instrument/instrument-type/instrument-type.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { InstrumentTypeService } from './../../../services/instrument/instrument-type.service';
+import { InstrumentType } from './../../../model/server/persistence/entity/instrument/instrument-type.class';
+import { MdSnackBar } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-instrument-type',
     templateUrl: './instrument-type.component.html',
     styleUrls: ['./instrument-type.component.scss']
 })
-export class InstrumentTypeComponent {
+export class InstrumentTypeComponent implements OnInit {
+
+	isDataLoaded:boolean;
+	instrumentTypeId:string;
+	instrumentType:InstrumentType = new InstrumentType();
+
+	constructor(
+		private instrumentTypeService:InstrumentTypeService,
+		private activatedRoute:ActivatedRoute,
+		private snackBar:MdSnackBar,
+	) {}
 	
+	ngOnInit() {
+		this.instrumentTypeId = this.activatedRoute.params['_value']['id'];
+		console.log(this.instrumentTypeId);
+		this.instrumentTypeService.getById((data) => {
+			console.log(data);
+			this.instrumentType = data;
+			this.isDataLoaded = true;
+		}, this.instrumentTypeId);
+	}
+
+	save() {
+		this.instrumentTypeService.update((data) => {
+			console.log(data);
+			this.snackBar.open("Equipment type saved.", "OK", {duration: 2000});
+		}, this.instrumentType);
+	}
+
 }

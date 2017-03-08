a94cffadffcd7446752fa8dba1ed6d753b91aece
Unknown <alvinquach91@gmail.com>
2017-03-08 14:29:15 -0800

    save

--- client/src/app/components/instrument/instrument-type-list/instrument-type-list.component.ts
+++ client/src/app/components/instrument/instrument-type-list/instrument-type-list.component.ts
@@ -1,4 +1,6 @@
-import { Sort } from './../../../utils/sort.utils';
+import { MdDialog, MdSnackBar } from '@angular/material';
+import { InstrumentTypeService } from './../../../services/instrument/instrument-type.service';
+import { Sort, SortUtils } from './../../../utils/sort.utils';
 import { InstrumentType } from './../../../model/server/persistence/entity/instrument/instrument-type.class';
 import { Component, OnInit } from '@angular/core';
 
@@ -19,8 +21,56 @@ export class InstrumentTypeListComponent implements OnInit {
 	}
 
 	sortProperties:any = {
-
+        type: [
+            "type"
+        ],
+        manufacturer: [
+            "manufacturer",
+            "type"
+        ],
+        instantaneous: [
+            "instantaneous",
+            "type"
+        ],
+        probe: [
+            "probe",
+            "type"
+        ],
+        methanePercent: [
+            "methanePercent",
+            "type"
+        ],
+        methanePpm: [
+            "methanePpm",
+            "type"
+        ],
+        hydrogenSulfidePpm: [
+            "hydrogenSulfidePpm",
+            "type"
+        ],
+        oxygenPercent: [
+            "oxygenPercent",
+            "type"
+        ],
+        carbonDioxidePercent: [
+            "carbonDioxidePercent",
+            "type"
+        ],
+        nitrogenPercent: [
+            "nitrogenPercent",
+            "type"
+        ],
+        pressure: [
+            "pressure",
+            "type"
+        ]
 	}
+
+    constructor(
+        private instrumentTypeService:InstrumentTypeService,
+        private dialog:MdDialog,
+		private snackBar:MdSnackBar
+    ) {}
 
     ngOnInit() {
 		this.loadingMessage = "Loading Equipment Types...";
@@ -28,7 +78,15 @@ export class InstrumentTypeListComponent implements OnInit {
 	}
 	
     loadInstrumentTypes() {
-
+        this.instrumentTypeService.getAll((data) => {
+            console.log(data);
+            this.instrumentTypeList = data;
+            this.isDataLoaded = true;
+        });
     }
+
+    sortBy(sortBy:string) {
+		SortUtils.sortAndUpdate(this.sort, sortBy, this.instrumentTypeList, this.sortProperties[sortBy]);
+	}
 
 }

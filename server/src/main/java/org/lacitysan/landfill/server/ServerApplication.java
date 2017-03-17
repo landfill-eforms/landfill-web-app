c39093a61cbe146778842fa7f81be799e44e0438
ligerx123 <ligerx066@gmail.com>
2017-03-16 20:19:37 -0700

    added code for war file

--- server/src/main/java/org/lacitysan/landfill/server/ServerApplication.java
+++ server/src/main/java/org/lacitysan/landfill/server/ServerApplication.java
@@ -6,16 +6,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author Alvin Quach
  */
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer{
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServerApplication.class);
+    }
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {

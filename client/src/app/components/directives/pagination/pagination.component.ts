003405b966f8c9b88c71cf7406baf69a2371914c
alvinquach <alvinquach91@gmail.com>
2017-04-02 04:18:27 -0700

    save

---
+++ client/src/app/components/directives/pagination/pagination.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnChanges } from '@angular/core';
+
+@Component({
+    selector: 'app-pagination',
+    templateUrl: './pagination.component.html',
+    styleUrls: ['./pagination.component.scss']
+})
+export class PaginationComponent implements OnChanges {
+
+	ngOnChanges() {
+
+	}
+
+}

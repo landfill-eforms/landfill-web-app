ed032d68ef618dc28fe78be8d875770c12b0cdf9
alvinquach <alvinquach91@gmail.com>
2017-04-17 18:09:30 -0700

    save

--- server/src/main/java/org/lacitysan/landfill/server/persistence/entity/serviceemission/integrated/IntegratedData.java
+++ server/src/main/java/org/lacitysan/landfill/server/persistence/entity/serviceemission/integrated/IntegratedData.java
@@ -1,103 +1,30 @@
 package org.lacitysan.landfill.server.persistence.entity.serviceemission.integrated;
 
-import java.sql.Timestamp;
-
+import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-import org.lacitysan.landfill.server.persistence.entity.instrument.Instrument;
-import org.lacitysan.landfill.server.persistence.entity.user.User;
-import org.lacitysan.landfill.server.persistence.enums.location.MonitoringPoint;
+import org.lacitysan.landfill.server.persistence.entity.serviceemission.ServiceEmissionData;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 /**
- * @author Allen Huang
  * @author Alvin Quach
  */
 @Entity
 @Table(name="dbo.IntegratedData")
+@AttributeOverride(name="id", column=@Column(name="IntegratedPK"))
 @JsonInclude(Include.NON_NULL)
-public class IntegratedData {
-	
-	@Id
-	@Column(name="IntegratedPK")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
-	
-	@Column(name="MonitoringPointString")
-	@Enumerated(EnumType.STRING)
-	private MonitoringPoint monitoringPoint;
-	
-	@ManyToOne
-	@JoinColumn(name="InstrumentFK")
-	private Instrument instrument;
-	
-	@JsonIgnoreProperties(value={"userGroups", "enabled"}, allowSetters=true)
-	@ManyToOne
-	@JoinColumn(name="InspectorFK")
-	private User inspector;
+public class IntegratedData extends ServiceEmissionData {
 	
 	@NotNull
 	private Short bagNumber;
 	
 	@NotNull
 	private Short volume;
-	
-	@NotNull
-	private Short barometricPressure;
-	
-	@NotNull
-	private Integer methaneLevel;
-	
-	@NotNull
-	private Timestamp startTime;
-	
-	@NotNull
-	private Timestamp endTime;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public MonitoringPoint getMonitoringPoint() {
-		return monitoringPoint;
-	}
-
-	public void setMonitoringPoint(MonitoringPoint monitoringPoint) {
-		this.monitoringPoint = monitoringPoint;
-	}
-
-	public Instrument getInstrument() {
-		return instrument;
-	}
-
-	public void setInstrument(Instrument instrument) {
-		this.instrument = instrument;
-	}
-
-	public User getInspector() {
-		return inspector;
-	}
-
-	public void setInspector(User inspector) {
-		this.inspector = inspector;
-	}
 
 	public Short getBagNumber() {
 		return bagNumber;
@@ -113,38 +40,6 @@ public class IntegratedData {
 
 	public void setVolume(Short volume) {
 		this.volume = volume;
-	}
-
-	public Short getBarometricPressure() {
-		return barometricPressure;
-	}
-
-	public void setBarometricPressure(Short barometricPressure) {
-		this.barometricPressure = barometricPressure;
-	}
-
-	public Integer getMethaneLevel() {
-		return methaneLevel;
-	}
-
-	public void setMethaneLevel(Integer methaneLevel) {
-		this.methaneLevel = methaneLevel;
-	}
-
-	public Timestamp getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(Timestamp startTime) {
-		this.startTime = startTime;
-	}
-
-	public Timestamp getEndTime() {
-		return endTime;
-	}
-
-	public void setEndTime(Timestamp endTime) {
-		this.endTime = endTime;
 	}
 	
 }

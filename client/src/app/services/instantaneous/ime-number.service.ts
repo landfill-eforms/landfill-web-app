e2bca54150c5306b63fe1f1dd9e8c9075894bb09
alvinquach <alvinquach91@gmail.com>
2017-05-04 03:45:10 -0700

    added methods to get only verified exceedance numbers

--- client/src/app/services/instantaneous/ime-number.service.ts
+++ client/src/app/services/instantaneous/ime-number.service.ts
@@ -1,3 +1,4 @@
+import { SurfaceExceedanceNumberService } from './../exceedance/surface-exceedance-number.service';
 import { AbstractHttpService } from './../abstract/abstract-http.service';
 import { ImeNumber } from './../../model/server/persistence/entity/surfaceemission/instantaneous/ime-number.class';
 import { Site } from './../../model/server/persistence/enums/location/site.enum';
@@ -8,17 +9,10 @@ import { Injectable } from '@angular/core';
 
 /** Handles the logical operations for ImeNumber objects. */
 @Injectable()
-export class ImeNumberService extends AbstractHttpService<ImeNumber> {
+export class ImeNumberService extends SurfaceExceedanceNumberService<ImeNumber> {
 
 	constructor(authHttp:AuthHttp) {
 		super('/ime-number', authHttp);
-	}
-	
-	getBySite(site:Site, success:(data) => void, error?:(err) => void) {
-		this.authHttp.get(this.baseUrl + "/list/site/" + site.constantName).map((res:Response) => res.json()).subscribe(
-				data => success(data),
-				err => error ? error(err) : console.log(err)
-			);
 	}
 
 	getByImeNumber(imeNumber:string, success:(data) => void, error?:(err) => void) {
@@ -26,13 +20,6 @@ export class ImeNumberService extends AbstractHttpService<ImeNumber> {
 				data => success(data),
 				err => error ? error(err) : console.log(err)
 			);
-	}
-
-	getStringFromImeNumber(imeNumber:ImeNumber):string {
-		let date:Date = new Date(imeNumber.dateCode);
-		let month = date.getMonth() + 1;
-		let dayOfMonth = date.getDate();
-        return imeNumber.site.shortName + "-" + (date.getFullYear() % 2000) + (month < 10 ? "0" + month : month) + "-" + (imeNumber.sequence < 10 ? "0" : "") + imeNumber.sequence;
 	}
 
 }

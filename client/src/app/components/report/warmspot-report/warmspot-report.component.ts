7bc47515015d5bfdcbe818656b0639aa1cc550bd
alvinquach <alvinquach91@gmail.com>
2017-05-12 07:30:14 -0700

    save

---
+++ client/src/app/components/report/warmspot-report/warmspot-report.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-warmspot-report',
+	templateUrl: './warmspot-report.component.html'
+})
+export class WarmspotReportComponent {
+
+}

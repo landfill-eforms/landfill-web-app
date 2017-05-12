2decdcf0d812dd1e44d5d287903c6ba0a2b73671
Allen Huang <ligerx066@gmail.com>
2017-05-12 07:25:51 -0700

    added warmspot pdf

---
+++ server/src/main/java/org/lacitysan/landfill/server/service/report/model/data/WarmspotReportData.java
@@ -0,0 +1,56 @@
+package org.lacitysan.landfill.server.service.report.model.data;
+
+public class WarmspotReportData {
+	private String monitoringPoint;
+	private String instrument;
+	private String inspector;
+	private String methane;
+	private String date;
+	private String description;
+	private String size;
+	
+	public String getMonitoringPoint() {
+		return monitoringPoint;
+	}
+	public void setMonitoringPoint(String monitoringPoint) {
+		this.monitoringPoint = monitoringPoint;
+	}
+	public String getInstrument() {
+		return instrument;
+	}
+	public void setInstrument(String instrument) {
+		this.instrument = instrument;
+	}
+	public String getInspector() {
+		return inspector;
+	}
+	public void setInspector(String inspector) {
+		this.inspector = inspector;
+	}
+	public String getMethane() {
+		return methane;
+	}
+	public void setMethane(String methane) {
+		this.methane = methane;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	
+	
+}

82b68cac7321f2af2edb092eeca64d54432b0aac
alvinquach <alvinquach91@gmail.com>
2017-05-12 07:33:59 -0700

    regenerated typescript classes

---
+++ client/src/app/model/server/service/report/model/data/warmspot-report-data.class.ts
@@ -0,0 +1,13 @@
+/**
+ * This class was automatically generated from WarmspotReportData.java using typescript-gen.
+ * https://github.com/landfill-eforms/landfill-tools/tree/master/typescript-gen
+ */
+export class WarmspotReportData {
+	monitoringPoint:string;
+	instrument:string;
+	inspector:string;
+	methane:string;
+	date:string;
+	description:string;
+	size:string;
+}

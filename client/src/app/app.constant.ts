e78bf01335e37280cc90789973514341260458de
alvinquach <alvinquach91@gmail.com>
2017-09-04 23:47:49 -0700

    updated resource server url

--- client/src/app/app.constant.ts
+++ client/src/app/app.constant.ts
@@ -1,8 +1,8 @@
 export class AppConstant {
 
-    static readonly RESOURCE_SERVER:string = "http://lef-backend.us-west-2.elasticbeanstalk.com";
+    static readonly RESOURCE_SERVER:string = "http://lef-server.us-west-2.elasticbeanstalk.com";
     
-    static readonly RESOURCE_SERVER_DEV:string = "http://localhost:5000";
+    static readonly RESOURCE_SERVER_DEV:string = "http://localhost:8080";
 
     static readonly RESTRICTED_ROUTE_BASE:string = "app";
 

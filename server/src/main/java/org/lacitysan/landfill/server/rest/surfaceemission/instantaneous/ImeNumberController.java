218cacb5b3765943fed181aeeb2719e5cd4fea77
alvinquach <alvinquach91@gmail.com>
2017-09-08 10:25:12 -0700

    feat(user role): assigned required roles to ime/ise controllers (#39)

--- server/src/main/java/org/lacitysan/landfill/server/rest/surfaceemission/instantaneous/ImeNumberController.java
+++ server/src/main/java/org/lacitysan/landfill/server/rest/surfaceemission/instantaneous/ImeNumberController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.lacitysan.landfill.server.config.app.ApplicationConstant;
 import org.lacitysan.landfill.server.persistence.dao.surfaceemission.instantaneous.ImeNumberDao;
 import org.lacitysan.landfill.server.persistence.entity.surfaceemission.instantaneous.ImeNumber;
+import org.lacitysan.landfill.server.persistence.enums.user.UserPermission;
+import org.lacitysan.landfill.server.security.annotation.RestAllowSuperAdminOnly;
+import org.lacitysan.landfill.server.security.annotation.RestSecurity;
 import org.lacitysan.landfill.server.service.surfaceemission.instantaneous.ImeNumberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,26 +29,31 @@ public class ImeNumberController {
 	@Autowired
 	ImeNumberService imeNumberService;
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/list/all", method=RequestMethod.GET)
 	public List<ImeNumber> getAll() {
 		return imeNumberDao.getAll();
 	}
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/list/verified", method=RequestMethod.GET)
 	public List<ImeNumber> getAllVerified() {
 		return imeNumberDao.getAllVerified();
 	}
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/list/unverified", method=RequestMethod.GET)
 	public List<ImeNumber> getAllUnverified() {
 		return imeNumberDao.getAllUnverified();
 	}
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/list/site/{siteName}", method=RequestMethod.GET)
 	public List<ImeNumber> getBySite(@PathVariable String siteName) {
 		return imeNumberService.getBySite(siteName);
 	}
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/list/site/{siteName}/dateCode/{dateCode}", method=RequestMethod.GET)
 	public List<ImeNumber> getBySiteAndDateCode(@PathVariable String siteName, @PathVariable String dateCode) {
 		try {
@@ -56,32 +64,38 @@ public class ImeNumberController {
 		}
 	}
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/unique/imeNumber/{imeNumber}", method=RequestMethod.GET)
 	public ImeNumber getByImeNumber(@PathVariable String imeNumber) {
 		ImeNumber temp = imeNumberService.generateImeNumberFromString(imeNumber);
 		return imeNumberDao.getByImeNumber(temp);
 	}
 	
+	@RestAllowSuperAdminOnly
 	@RequestMapping(value="/create", method=RequestMethod.POST)
 	public ImeNumber create(@RequestBody ImeNumber imeNumber) {
 		return imeNumberService.createUnverified(imeNumber);
 	}
 	
+	@RestSecurity(UserPermission.EDIT_EXCEEDANCES)
 	@RequestMapping(value="/update", method=RequestMethod.POST)
 	public ImeNumber update(@RequestBody ImeNumber imeNumber) {
 		return imeNumberService.update(imeNumber);
 	}
 	
+	@RestSecurity(UserPermission.EDIT_EXCEEDANCES)
 	@RequestMapping(value="/clear", method=RequestMethod.POST)
 	public ImeNumber clear(@RequestBody ImeNumber imeNumber) {
 		return imeNumberService.clear(imeNumber);
 	}
 
+	@RestAllowSuperAdminOnly
 	@RequestMapping(value="/delete", method=RequestMethod.POST)
 	public ImeNumber delete(@RequestBody ImeNumber imeNumber) {
 		return imeNumberDao.delete(imeNumber);
 	}
 
+	@RestAllowSuperAdminOnly
 	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
 	public ImeNumber deleteById(@PathVariable String id) {
 		try {
--- server/src/main/java/org/lacitysan/landfill/server/rest/surfaceemission/integrated/IseNumberController.java
+++ server/src/main/java/org/lacitysan/landfill/server/rest/surfaceemission/integrated/IseNumberController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.lacitysan.landfill.server.config.app.ApplicationConstant;
 import org.lacitysan.landfill.server.persistence.dao.surfaceemission.integrated.IseNumberDao;
 import org.lacitysan.landfill.server.persistence.entity.surfaceemission.integrated.IseNumber;
+import org.lacitysan.landfill.server.persistence.enums.user.UserPermission;
+import org.lacitysan.landfill.server.security.annotation.RestAllowSuperAdminOnly;
+import org.lacitysan.landfill.server.security.annotation.RestSecurity;
 import org.lacitysan.landfill.server.service.surfaceemission.integrated.IseNumberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,26 +29,31 @@ public class IseNumberController {
 	@Autowired
 	IseNumberService iseNumberService;
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/list/all", method=RequestMethod.GET)
 	public List<IseNumber> getAll() {
 		return iseNumberDao.getAll();
 	}
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/list/verified", method=RequestMethod.GET)
 	public List<IseNumber> getAllVerified() {
 		return iseNumberDao.getAllVerified();
 	}
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/list/unverified", method=RequestMethod.GET)
 	public List<IseNumber> getAllUnverified() {
 		return iseNumberDao.getAllUnverified();
 	}
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/list/site/{siteName}", method=RequestMethod.GET)
 	public List<IseNumber> getBySite(@PathVariable String siteName) {
 		return iseNumberService.getBySite(siteName);
 	}
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/list/site/{siteName}/dateCode/{dateCode}", method=RequestMethod.GET)
 	public List<IseNumber> getBySiteAndDateCode(@PathVariable String siteName, @PathVariable String dateCode) {
 		try {
@@ -56,32 +64,38 @@ public class IseNumberController {
 		}
 	}
 	
+	@RestSecurity(UserPermission.VIEW_EXCEEDANCES)
 	@RequestMapping(value="/unique/iseNumber/{iseNumber}", method=RequestMethod.GET)
 	public IseNumber getByIseNumber(@PathVariable String iseNumber) {
 		IseNumber temp = iseNumberService.generateIseNumberFromString(iseNumber);
 		return iseNumberDao.getByIseNumber(temp);
 	}
 	
+	@RestAllowSuperAdminOnly
 	@RequestMapping(value="/create", method=RequestMethod.POST)
 	public IseNumber create(@RequestBody IseNumber iseNumber) {
 		return iseNumberService.createUnverified(iseNumber);
 	}
 	
+	@RestSecurity(UserPermission.EDIT_EXCEEDANCES)
 	@RequestMapping(value="/update", method=RequestMethod.POST)
 	public IseNumber update(@RequestBody IseNumber iseNumber) {
 		return iseNumberService.update(iseNumber);
 	}
 	
+	@RestSecurity(UserPermission.EDIT_EXCEEDANCES)
 	@RequestMapping(value="/clear", method=RequestMethod.POST)
 	public IseNumber clear(@RequestBody IseNumber iseNumber) {
 		return iseNumberService.clear(iseNumber);
 	}
 
+	@RestAllowSuperAdminOnly
 	@RequestMapping(value="/delete", method=RequestMethod.POST)
 	public IseNumber delete(@RequestBody IseNumber iseNumber) {
 		return iseNumberDao.delete(iseNumber);
 	}
 
+	@RestAllowSuperAdminOnly
 	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
 	public IseNumber deleteById(@PathVariable String id) {
 		try {

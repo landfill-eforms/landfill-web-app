4baf8d5f91d380c9ebf9cc4d455b4dd5a9cb9d85
Jose Gonzalez <jgonz219@gmail.com>
2017-10-12 20:52:58 -0700

    added multiple grid to warmspot dialog

--- client/src/app/components/unverified-data/dialog/edit-unverified-warmspot-dialog/edit-unverified-warmspot-dialog.component.ts
+++ client/src/app/components/unverified-data/dialog/edit-unverified-warmspot-dialog/edit-unverified-warmspot-dialog.component.ts
@@ -1,3 +1,6 @@
+import { Site } from './../../../../model/server/persistence/enums/location/site.enum';
+import { MonitoringPointType } from './../../../../model/server/persistence/enums/location/monitoring-point-type.enum';
+import { MonitoringPoint } from './../../../../model/server/persistence/enums/location/monitoring-point.enum';
 import { UnverifiedWarmspotData } from './../../../../model/server/persistence/entity/unverified/unverified-warmspot-data.class';
 import { MdDialogRef, MdSnackBar } from '@angular/material';
 import { Instrument } from './../../../../model/server/persistence/entity/instrument/instrument.class';
@@ -10,15 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUnverifiedWarmspotDialogComponent implements OnInit {
 
+	site:Site;
 	data:UnverifiedWarmspotData;
 	availableInstruments:Instrument[] = [];
+	monitoringPointsWrapped:{monitoringPoint:MonitoringPoint, selected?:boolean} [] = [];
 
 	fields:{
 		instrumentId?:number, 
 		methaneLevel?:number, 
 		size?:string, 
 		description?:string,
-		grid?:string
+		monitoringPoints?:MonitoringPoint[]
 	} = {}
 
 	constructor(
@@ -31,11 +36,22 @@ export class EditUnverifiedWarmspotDialogComponent implements OnInit {
 		this.fields.methaneLevel = this.data.methaneLevel / 100;
 		this.fields.size = this.data.size;
 		this.fields.description = this.data.description;
+
 		this.availableInstruments = this.availableInstruments.filter(i => i.instrumentType.instantaneous);
-		this.fields.grid = this.data.monitoringPoint.name;
+		if (this.data) {
+			let selectedGrids: number [] = this.data.monitoringPoints.map(g => g.ordinal);
+            let monitoringPoints = MonitoringPoint.values().filter(g => {
+                return g.site.ordinal == this.site.ordinal &&
+                    g.type.ordinal == MonitoringPointType.GRID.ordinal;
+			});
+			for (let monitoringPoint of monitoringPoints) {
+                this.monitoringPointsWrapped.push({monitoringPoint: monitoringPoint, selected: selectedGrids.indexOf(monitoringPoint.ordinal) > -1});
+            }
+		}
 	}
 
 	confirm() {
+		this.fields.monitoringPoints = this.monitoringPointsWrapped.filter(g => g.selected).map(g => g.monitoringPoint);
 		this.dialogRef.close(this.fields);
 	}
 
@@ -44,6 +60,6 @@ export class EditUnverifiedWarmspotDialogComponent implements OnInit {
 	}
 
 	canSubmit(): boolean{
-		return this.fields.grid != null && this.fields.instrumentId != null && this.fields.methaneLevel != null && !!this.fields.size;
+		return !!this.monitoringPointsWrapped.filter(g => g.selected).length && this.fields.instrumentId != null && this.fields.instrumentId != null && this.fields.methaneLevel != null && !!this.fields.size;
 	}
 }
